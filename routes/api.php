569e233cdf5188bb7440882fc4e855cf725e00a1
Swoyam <swoyamsuwal07@gmail.com>
2025-01-24 20:01:24 +0545

    API for HoodieApi

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Example API route
-Route::get('/test', function () {
-    return response()->json(['message' => 'API is working!']);
-});
+use App\Http\Controllers\Api\HoodieApiController;
+
+Route::get('hoodies', [HoodieApiController::class, 'getHoodies']);
+
