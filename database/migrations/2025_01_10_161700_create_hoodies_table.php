0800ba5d1bb50a9d507787a7e871d686135809bf
Swoyam <swoyamsuwal07@gmail.com>
2025-01-24 16:28:08 +0545

    Admin Password

--- database/migrations/2025_01_10_161700_create_hoodies_table.php
+++ database/migrations/2025_01_10_161700_create_hoodies_table.php
@@ -10,10 +10,10 @@ class CreateHoodiesTable extends Migration
     {
         Schema::create('hoodies', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('slug')->nullable();
-            $table->text('description');
-            $table->text('meta_description');
+            $table->text('description')->nullable();
+            $table->text('meta_description')->nullable();
             $table->string('main_image')->nullable();
             $table->string('mobile_image')->nullable();
             $table->timestamps();
